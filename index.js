71e62948da15cf1ba9b4d4e71983949aae842562
RobFl4 <37384310+RobFl4@users.noreply.github.com>
2021-01-15 16:26:52 +0100

    Initial version

---
+++ index.js
@@ -0,0 +1,28 @@
+// var myLength = prompt('length');
+// var myWeigth = prompt('weight');
+// document.firstElement
+// <input type="text" class="textInput" data-testid="length" name="tekst" id="length" value="length">
+// /html/body/input[1]
+// /html/body/input[2]
+// /html/body/input[3]
+// function myFunction() {
+//   if (document.querySelector("#length").value < 120) {
+//      document.querySelector("#length").style.color = "red";
+//      document.getElementById("demo").style.color = "red";
+//      document.getElementById("demo").innerHTML = "Opnieuw!";
+//      }
+//      else {
+//   document.getElementById("demo").style.color = "green";
+//   document.querySelector("#length").style.color = "green";
+//   document.getElementById("demo").innerHTML = "Goed gedaan!";
+//   }
+// }
+
+// if (document.querySelector("#length").value < 120) {
+   document.querySelector("#length").style.color = "blue";
+   document.querySelector("#weight").style.color = "blue";
+   document.querySelector("#kilometerstand").style.color = "blue";
+   // document.querySelector("#voorruit").style.color = "blue";
+   // document.querySelector("#banden").style.color = "blue";
+   // document.querySelector("#licht").style.color = "blue";
+   // }
